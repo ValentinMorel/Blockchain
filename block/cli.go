88e794c8a832ae7670afbbffa62bedfe17594405
Admin <valntn.mrl@gmail.com>
2020-08-02 12:32:13 +0200

    Refactor into a single package "block".

---
+++ block/cli.go
@@ -0,0 +1,84 @@
+package block
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+)
+
+type CommandLine struct {
+	Blockchain *BlockChain
+}
+
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println(" add -block BLOCK_DATA - add a block to the chain")
+	fmt.Println(" print - Prints the blocks in the chain")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.Blockchain.AddBlock(data)
+	fmt.Println("Added Block!")
+}
+
+func (cli *CommandLine) printChain() {
+	iter := cli.Blockchain.Iterator()
+
+	for {
+		blck := iter.Next()
+
+		fmt.Printf("Prev. hash: %x\n", blck.PrevHash)
+		fmt.Printf("Data: %s\n", blck.Data)
+		fmt.Printf("Hash: %x\n", blck.Hash)
+		pow := NewProof(blck)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(blck.PrevHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CommandLine) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("block", "", "Block data")
+
+	switch os.Args[1] {
+	case "add":
+		err := addBlockCmd.Parse(os.Args[2:])
+		Handle(err)
+
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		Handle(err)
+
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
